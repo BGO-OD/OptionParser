6cf46b85d175e5290d8d570cda1bd950e9fbee25
Juergen Hannappel <juergen@juergen-hannappel.de>
2016-05-30 17:44:52 +0000

    Add appveyor integration to test build on MS Windows using MSYS2.
    Strongly inspired by the appveyor integration of mpv.

---
+++ appveyor-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+set -e
+
+export DEST_OS=win32
+export CC=gcc
+export PKG_CONFIG=/usr/bin/pkg-config
+export TOPDIR=$(pwd)
+
+mkdir ${TOPDIR}/build
+mkdir ${TOPDIR}/install
+
+export PATH=${PATH}:${TOPDIR}/install/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TOPDIR}/install/lib
+
+cd ${TOPDIR}/build
+cmake -DCMAKE_INSTALL_PREFIX:PATH=${TOPDIR}/install -DOptionParser_INSTALL_EXAMPLES=ON ${TOPDIR}/
+make
+make install
+cd ${TOPDIR}/install
+optionExample --help
+${TOPDIR}/src/shellScriptExample --help

6960770d864108fbf96fe1e60035ddfed4fc689f
Juergen Hannappel <juergen@juergen-hannappel.de>
2016-05-30 17:44:43 +0000

    cmake: Factor out version-detection (from latest tag) and pkgconfig.
    This cleans the main cmake-lists a bit and allows for further extensions.

---
+++ cmake/WritePkgConfig.cmake
@@ -0,0 +1,11 @@
+# Create pkg-config file.
+
+# convert lists of link libraries into -lstdc++ -lm etc..
+foreach(LIB ${CMAKE_CXX_IMPLICIT_LINK_LIBRARIES} ${PLATFORM_LIBS})
+  set(PRIVATE_LIBS "${PRIVATE_LIBS} -l${LIB}")
+endforeach()
+# Produce a pkg-config file for linking against the shared lib
+configure_file("${CMAKE_PROJECT_NAME}.pc.in" "${CMAKE_PROJECT_NAME}.pc" @ONLY)
+install(FILES
+	"${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_PROJECT_NAME}.pc"
+  DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig")

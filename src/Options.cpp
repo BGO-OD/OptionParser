ec29659c3b34da51d1a13a112540fb8b031874c3
Dr. Jürgen Hannappel <juergen@juergen-hannappel.de>
2017-04-03 11:33:50 +0200

    define _GNU_SOURCE, might help with mingw

--- src/Options.cpp
+++ src/Options.cpp
@@ -15,6 +15,10 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include "Options.h"
 #include <sys/types.h>
 #include <sys/stat.h>
